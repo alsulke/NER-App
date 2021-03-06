0e49b02c7151fdfacac2ea8ba5c1056f2fb61394
Aniket Laxman Sulke <46842984+alsulke@users.noreply.github.com>
2021-03-07 00:29:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,22 +9,25 @@ def main() :
 	st.title("Streamlit App for NER")
 	menu = ["NER - WIKI API", "NER - TEXT"]
 	choice = st.sidebar.selectbox("Menu", menu)
+	
+	try:
+		if(choice == "NER - WIKI API") : # Extract data using API endpoint
+			st.subheader("Named Entity Recognition using Wiki API")
+			raw_text = st.text_area("Your text", "Enter text")
 
-	if(choice == "NER - WIKI API") : # Extract data using API endpoint
-		st.subheader("Named Entity Recognition using Wiki API")
-		raw_text = st.text_area("Your text", "Enter text")
+			context = requests.get(f"https://flask-api-01.herokuapp.com/{raw_text}")
+			context = context.json()
+			print(context['data'])
+			docx = nlp(context['data'])
 
-		context = requests.get(f"https://flask-api-01.herokuapp.com/{raw_text}")
-		context = context.json()
-		print(context['data'])
-		docx = nlp(context['data'])
-
-		spacy_streamlit.visualize_ner(docx,labels=nlp.get_pipe('ner').labels)
-	elif(choice == "NER - TEXT") : # Type text manually
-		st.subheader("Named Entity Recognition using custom text")
-		raw_text = st.text_area("Your text", "Enter text")
-		docx = nlp(raw_text)
-		spacy_streamlit.visualize_ner(docx,labels=nlp.get_pipe('ner').labels)
+			spacy_streamlit.visualize_ner(docx,labels=nlp.get_pipe('ner').labels)
+		elif(choice == "NER - TEXT") : # Type text manually
+			st.subheader("Named Entity Recognition using custom text")
+			raw_text = st.text_area("Your text", "Enter text")
+			docx = nlp(raw_text)
+			spacy_streamlit.visualize_ner(docx,labels=nlp.get_pipe('ner').labels)
+	except: 
+		spacy_streamlit.visualize_ner("Oops thats not found! Try some different ")
 
 
 if __name__ == '__main__':
